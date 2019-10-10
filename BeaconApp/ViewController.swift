883db30e6b82f2fea282e3e702335f403b3f418f
Anup Deshpande <deshpande.anup24@gmail.com>
2019-10-09 20:24:13 -0400

    Initial Commit

---
+++ BeaconApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeaconApp
+//
+//  Created by Anup Deshpande on 10/9/19.
+//  Copyright © 2019 Anup Deshpande. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
